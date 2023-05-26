bac434e3ec50fa29c806b2c28a26311d2b46dcf1
Vlad Bekker <vlad.bekker@jackpocket.com>
2023-05-26 17:18:25 -0400

    additional file for gitpull

---
+++ snmp_poller/github_pull_backuptest.py
@@ -0,0 +1,16 @@
+import os
+import subprocess
+
+#	git config --global --add safe.directory /home/datadog/ops-neteng-public
+
+os.chdir("/home/datadog/ops-neteng-public")
+
+result = subprocess.check_output(["sudo", "git", "pull"])
+
+coolresult = (str(result))
+
+if "Already up to date." in coolresult:
+    pass
+else:
+    #print("Need to execute the installer")
+    os.system('/usr/bin/python3 /home/datadog/ops-neteng-public/backup_speedtest/backup_speedtest_installer.py')
